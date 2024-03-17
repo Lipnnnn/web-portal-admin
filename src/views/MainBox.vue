22fa5dd66755f9aef28d1907bb88f36d6ad92096
lipnGit <lipn2002@163.com>
2024-03-17 15:57:51 +0800

    完成了首页的头部header模块

--- src/views/MainBox.vue
+++ src/views/MainBox.vue
@@ -7,7 +7,7 @@
           class="el-menu"
           :router="true"
           :collapse="collapse"
-          :default-active="'/index'"
+          :default-active="route.fullPath"
           >
             <el-menu-item index="/index">
               <el-icon><home-filled /></el-icon>
@@ -46,8 +46,27 @@
       </el-aside>
       <el-container>
         <el-header>
-          <button @click="isCollapse">click</button>
-          Header
+          <div class="el-header-text">
+            <el-icon @click="isCollapse" :size="size" :color="color"><Menu /></el-icon>
+            <h2 class="el-header-text-title">Lipn门户网站管理系统</h2>
+          </div>
+          <div class="el-header-text">
+            <span>欢迎 admin 回来</span>
+            <el-dropdown>
+              <span class="el-dropdown-link">
+                <el-icon color="white" size="30"><User /></el-icon>
+                <el-icon class="el-icon--right">
+                  <arrow-down />
+                </el-icon>
+              </span>
+              <template #dropdown>
+                <el-dropdown-menu>
+                  <el-dropdown-item @click="handleCenter">个人中心</el-dropdown-item>
+                  <el-dropdown-item @click="handleLogout">退出登录</el-dropdown-item>
+                </el-dropdown-menu>
+              </template>
+            </el-dropdown>
+          </div>
         </el-header>
         <el-main>
           <router-view></router-view>
@@ -58,8 +77,11 @@
 </template>
 
 <script setup>
-import { HomeFilled,Avatar,UserFilled,MessageBox,Reading } from '@element-plus/icons-vue';
+import { HomeFilled,Avatar,UserFilled,MessageBox,Reading,Menu,User } from '@element-plus/icons-vue';
 import {ref} from 'vue'
+import { useRoute,useRouter } from 'vue-router';
+const route = useRoute();  // route是当前匹配到的路由对象
+const router = useRouter();  // router是整个路由器对象
 
 // 设置是否折叠
 let collapse = ref(false);
@@ -67,6 +89,15 @@ const isCollapse = ()=>{
   collapse.value = !collapse.value;
 }
 
+// 点击个人中心
+const handleCenter = ()=>{
+  router.push('/center');
+}
+// 点击退出登录
+const handleLogout = ()=>{
+  localStorage.removeItem('token');
+  router.push('/login');
+}
 
 </script>
 
@@ -75,6 +106,19 @@ const isCollapse = ()=>{
   height: 100vh;
   .el-menu{
     height: 100vh;
+  }
+}
+.el-header{
+  display: flex;
+  justify-content: space-between;
+  background-color: #2c3a4a;
+  color: white;
+  .el-header-text{
+    display: flex;
+    align-items: center;
+    .el-header-text-title,.el-dropdown{
+      margin-left: 10px;
+    }
   }
 }
 
